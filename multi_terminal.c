e03e5d9d8e7fc03d80f914d0459640c9c92dd17b
tharindra galahena <tcg.galahena@gmail.com>
2012-05-24 20:40:08 +0530

    c4

--- multi_terminal.c
+++ multi_terminal.c
@@ -6,6 +6,7 @@
 
 #include <gtk/gtk.h>
 #include <vte/vte.h> 
+#include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
 
@@ -15,6 +16,10 @@ GtkWidget *box_main;
 GtkWidget *scale_opt;
 GtkWidget *button_bcolor;
 GtkWidget *button_fcolor;
+
+GdkColor bcolor;
+GdkColor fcolor;
+
 int c = 1;
 
 struct v{
@@ -26,6 +31,10 @@ struct v{
 struct v *list = NULL;
 struct v *st = NULL;
 
+void set_data(struct v *p){
+	vte_terminal_set_color_background(VTE_TERMINAL(p-> vte), &bcolor);
+	vte_terminal_set_color_foreground(VTE_TERMINAL(p-> vte), &fcolor);
+}
 void add_vte(){
 	struct v *temp = (struct v *)malloc(sizeof(struct v));
 	temp-> vte = vte_terminal_new();
@@ -34,6 +43,7 @@ void add_vte(){
 	vte_terminal_set_size(VTE_TERMINAL(temp-> vte), 80, 5);
 	gtk_box_pack_start (GTK_BOX (box_vte), temp-> vte, FALSE, TRUE, 5);
 	gtk_widget_show (temp-> vte);
+	set_data(temp);
 	temp -> next = list -> next;
 	(list -> next) -> prev = temp;
 	list -> next = temp;
@@ -45,12 +55,14 @@ void remove_vte(){
 	if (c > 1){
 		if(list == st) list = list -> prev;
 		gtk_container_remove (GTK_CONTAINER (box_vte), st -> vte);
+		struct v *r = st;
 		st = st -> next;
 		st -> prev = (st -> prev) -> prev;
 		(st -> prev) -> next = st;
 		vte_terminal_set_size(VTE_TERMINAL(st-> vte), 80, 20);
 		c--;
 		gtk_window_resize (GTK_WINDOW(window_main), 20, 20);
+		free(r);
 	}
 }
 void switch_vte(){
@@ -60,23 +72,66 @@ void switch_vte(){
 		vte_terminal_set_size(VTE_TERMINAL(st-> vte), 80, 20);
 	}
 }
+void write_config(){
+	FILE *fp;
+	fp = fopen("./config", "wa");
+
+	char *color = gdk_color_to_string (&bcolor);
+	printf("%s\n", color);
+	fwrite(color , 1, 13, fp);
+	free(color);
+
+	color = gdk_color_to_string (&fcolor);
+	printf("%s\n", color);
+	fwrite(color , 1, 13, fp);
+	free(color);
+	
+	fclose(fp);
+}
+void read_config(){
+	FILE *fp;
+	fp = fopen("./config", "ra");
+
+	char *color = malloc(14);
+	
+	fread(color , 1, 13, fp);
+	printf("%s\n", color);
+	gdk_color_parse (color, &bcolor);
+	free(color);
+	
+	color = malloc(14);
+	fread(color , 1, 13, fp);
+	printf("%s\n", color);
+	gdk_color_parse (color, &fcolor);
+	free(color);
+	
+	fclose(fp);
+	set_data(st);
+}
 void save(){
-	GdkColor bcolor;
+	
 	gtk_color_button_get_color(GTK_COLOR_BUTTON(button_bcolor), &bcolor);
-	vte_terminal_set_color_background(VTE_TERMINAL(st-> vte), &bcolor);
-	
-	GdkColor fcolor;
 	gtk_color_button_get_color(GTK_COLOR_BUTTON(button_fcolor), &fcolor);
-	vte_terminal_set_color_foreground(VTE_TERMINAL(st-> vte), &fcolor);
-	
-	//vte_terminal_set_background_transparent(VTE_TERMINAL(st-> vte), TRUE);
+
+	struct v *p = st;
+	int i;
+	for(i = 0; i < c; i++){
+		set_data(p);
+		p = p -> next;
+	}
+	gtk_color_button_set_color(GTK_COLOR_BUTTON(button_bcolor), &bcolor);
+	gtk_color_button_set_color(GTK_COLOR_BUTTON(button_fcolor), &fcolor);
+	write_config();
 }
 void settings(){
 	GtkWidget *window_settings;
 	
-	//GtkWidget *box_main;
 	GtkWidget *box_buttons;
-	GtkWidget *box_sliders;
+	GtkWidget *box_cbuttons1;
+	GtkWidget *box_cbuttons2;
+	
+	GtkWidget * label_bg;
+	GtkWidget * label_fg;
 	
 	GtkWidget* button_save;
 	GtkWidget* button_close;
@@ -84,17 +139,20 @@ void settings(){
 	window_settings = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	box_main = gtk_vbox_new(FALSE, 0);
 	box_buttons = gtk_hbox_new(FALSE, 0);
-	box_sliders = gtk_vbox_new(FALSE, 0);
+	box_cbuttons1 = gtk_hbox_new(FALSE, 0);
+	box_cbuttons2 = gtk_hbox_new(FALSE, 0);
 	
+	label_bg = gtk_label_new("Background  : ");   
+	gtk_box_pack_start (GTK_BOX(box_cbuttons1), label_bg, TRUE, FALSE, 5);
 	button_bcolor = gtk_color_button_new();
-	gtk_box_pack_start (GTK_BOX (box_sliders), button_bcolor, TRUE, FALSE, 5);
+	gtk_box_pack_start (GTK_BOX(box_cbuttons1), button_bcolor, TRUE, FALSE, 5);
+	gtk_box_pack_start (GTK_BOX(box_main), box_cbuttons1, TRUE, FALSE, 5);
 	
+	label_fg = gtk_label_new("Foreground  : ");   
+	gtk_box_pack_start (GTK_BOX(box_cbuttons2), label_fg, TRUE, FALSE, 5);
 	button_fcolor = gtk_color_button_new();
-	gtk_box_pack_start (GTK_BOX (box_sliders), button_fcolor, TRUE, FALSE, 5);
-	//scale_opt = gtk_hscale_new_with_range(0, 65535, 10);
-	
-	//gtk_box_pack_start (GTK_BOX (box_sliders), scale_opt, TRUE, FALSE, 5);
-	gtk_box_pack_start (GTK_BOX (box_main), box_sliders, TRUE, FALSE, 5);
+	gtk_box_pack_start (GTK_BOX(box_cbuttons2), button_fcolor, TRUE, FALSE, 5);
+	gtk_box_pack_start (GTK_BOX(box_main), box_cbuttons2, TRUE, FALSE, 5);
 	
 	button_save = gtk_button_new_with_label("Save");
 	button_close = gtk_button_new_with_label("Close");
@@ -108,21 +166,23 @@ void settings(){
 	gtk_box_pack_start (GTK_BOX (box_main), box_buttons, TRUE, FALSE, 5);
 	gtk_container_add (GTK_CONTAINER (window_settings), box_main);
 	
-	gtk_widget_show (box_sliders);
+	gtk_widget_show (label_fg);
+	gtk_widget_show (label_bg);
+	gtk_widget_show (box_cbuttons1);
+	gtk_widget_show (box_cbuttons2);
 	gtk_widget_show (button_bcolor);
 	gtk_widget_show (button_fcolor);
-	//gtk_widget_show (scale_opt);
+
 	gtk_widget_show (button_save);
 	gtk_widget_show (button_close);
 	gtk_widget_show (box_buttons);
 	gtk_widget_show (box_main);
 	gtk_widget_show (window_settings);
+	gtk_color_button_set_color(GTK_COLOR_BUTTON(button_bcolor), &bcolor);
+	gtk_color_button_set_color(GTK_COLOR_BUTTON(button_fcolor), &fcolor);
 }
 int main(int argc, char **argv){
-	
-	//GtkWidget *window_main;
-	
-	//GtkWidget *box_main;
+
 	GtkWidget *box_buttons;
 	
 	GtkWidget* button_new;
@@ -151,6 +211,7 @@ int main(int argc, char **argv){
 	vte_terminal_fork_command(VTE_TERMINAL(st -> vte), "bash", 
 		NULL, NULL, ".", FALSE, FALSE, FALSE); 
 	vte_terminal_set_size(VTE_TERMINAL(st -> vte), 80,20);
+	
 	
 	gtk_signal_connect (GTK_OBJECT (button_new), "clicked",
 					GTK_SIGNAL_FUNC (add_vte), NULL);
@@ -181,6 +242,7 @@ int main(int argc, char **argv){
 	gtk_widget_show(box_buttons);
 	gtk_widget_show(box_vte);
 	gtk_widget_show(window_main);
+	read_config();
 	gtk_main();
 	return 0;
 }
